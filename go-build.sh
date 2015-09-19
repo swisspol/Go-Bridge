6ebb2626a50c295098cde3ad865f947263015522
Pierre-Olivier Latour <pol@mac.com>
2015-09-18 21:33:22 -0700

    Compile entire go directory

--- go-build.sh
+++ go-build.sh
@@ -5,6 +5,8 @@ source "env.sh"
 BUILD_DIR="`pwd`/build/$PLATFORM_NAME"
 mkdir -p "$BUILD_DIR"
 
+cd "go"
+
 if [ "$PLATFORM_NAME" == "macosx" ]
 then
   (
@@ -13,7 +15,7 @@ then
     export GOARCH=amd64
     export CGO_CFLAGS="-isysroot $MACOSX_SDK_PATH -arch x86_64 -mmacosx-version-min=$OSX_MIN_VERSION"
     export CGO_LDFLAGS="-isysroot $MACOSX_SDK_PATH -arch x86_64 -mmacosx-version-min=$OSX_MIN_VERSION"
-    go build -pkgdir="$GOROOT/pkg_cross/MacOSX_x86_64" -v -x -buildmode=c-archive -o "$BUILD_DIR/main.a" "go/main.go"
+    go build -pkgdir="$GOROOT/pkg_cross/MacOSX_x86_64" -v -x -buildmode=c-archive -o "$BUILD_DIR/main.a"
   )
 elif [ "$PLATFORM_NAME" == "iphonesimulator" ]
 then
@@ -23,7 +25,7 @@ then
   #   export GOARCH=386
   #   export CGO_CFLAGS="-isysroot $IPHONESIMULATOR_SDK_PATH -arch i386 -mios-simulator-version-min=$IOS_MIN_VERSION"
   #   export CGO_LDFLAGS="-isysroot $IPHONESIMULATOR_SDK_PATH -arch i386 -mios-simulator-version-min=$IOS_MIN_VERSION"
-  #   go build -pkgdir="$GOROOT/pkg_cross/iPhoneSimulator_i386" -tags=ios -v -x -buildmode=c-archive -o "$BUILD_DIR/main_32.a" "go/main.go"
+  #   go build -pkgdir="$GOROOT/pkg_cross/iPhoneSimulator_i386" -tags=ios -v -x -buildmode=c-archive -o "$BUILD_DIR/main_32.a"
   # )
   (
     export CGO_ENABLED=1
@@ -31,7 +33,7 @@ then
     export GOARCH=amd64
     export CGO_CFLAGS="-isysroot $IPHONESIMULATOR_SDK_PATH -arch x86_64 -mios-simulator-version-min=$IOS_MIN_VERSION"
     export CGO_LDFLAGS="-isysroot $IPHONESIMULATOR_SDK_PATH -arch x86_64 -mios-simulator-version-min=$IOS_MIN_VERSION"
-    go build -pkgdir="$GOROOT/pkg_cross/iPhoneSimulator_x86_64" -tags=ios -v -x -buildmode=c-archive -o "$BUILD_DIR/main_64.a" "go/main.go"
+    go build -pkgdir="$GOROOT/pkg_cross/iPhoneSimulator_x86_64" -tags=ios -v -x -buildmode=c-archive -o "$BUILD_DIR/main_64.a"
   )
   
   # TODO: Building for iOS Simulator on i386 does not work
@@ -51,7 +53,7 @@ then
     export GOARM=7
     export CGO_CFLAGS="-isysroot $IPHONEOS_SDK_PATH -arch armv7 -miphoneos-version-min=$IOS_MIN_VERSION"
     export CGO_LDFLAGS="-isysroot $IPHONEOS_SDK_PATH -arch armv7 -miphoneos-version-min=$IOS_MIN_VERSION"
-    go build -pkgdir="$GOROOT/pkg_cross/iPhoneOS_armv7" -tags=ios -v -x -buildmode=c-archive -o "$BUILD_DIR/main_32.a" "go/main.go"
+    go build -pkgdir="$GOROOT/pkg_cross/iPhoneOS_armv7" -tags=ios -v -x -buildmode=c-archive -o "$BUILD_DIR/main_32.a"
   )
   (
     export CGO_ENABLED=1
@@ -59,7 +61,7 @@ then
     export GOARCH=arm64
     export CGO_CFLAGS="-isysroot $IPHONEOS_SDK_PATH -arch arm64 -miphoneos-version-min=$IOS_MIN_VERSION"
     export CGO_LDFLAGS="-isysroot $IPHONEOS_SDK_PATH -arch arm64 -miphoneos-version-min=$IOS_MIN_VERSION"
-    go build -pkgdir="$GOROOT/pkg_cross/iPhoneOS_arm64" -tags=ios -v -x -buildmode=c-archive -o "$BUILD_DIR/main_64.a" "go/main.go"
+    go build -pkgdir="$GOROOT/pkg_cross/iPhoneOS_arm64" -tags=ios -v -x -buildmode=c-archive -o "$BUILD_DIR/main_64.a"
   )
   
   printf "#ifdef __LP64__\n#include \"main_64.h\"\n#else\n#include \"main_32.h\"\n#endif\n" > "$BUILD_DIR/main.h"
